3f11611086a3518ccdc953a6d5915e6f8912d301
Thomas Brady <thomas.brady010@gmail.com>
2025-09-07 21:07:59 -0400

    updates

--- task_tracker.py
+++ task_tracker.py
@@ -10,6 +10,17 @@ if not path.exists():
     with open(path, 'w') as f:
         json.dump(task_list,f)
 
+# load tasks file
+def load_files():
+    with open(path,'r') as f:
+        tasks = json.load(f)
+    return tasks
+
+# save tasks 
+def save_files(tasks):
+    with open(path,'w') as f:
+        json.dump(tasks,f,indent=2)
+
 # main menu
 menu = """Task List - Main Menu:
 1.) Add, Update, or Delete tasks
@@ -21,46 +32,37 @@ menu = """Task List - Main Menu:
 7.) Close program"""
 
 def listTasks():
-    with open(path, 'r') as f:
-        tasks = json.load(f)
+    tasks = load_files()
     for task in tasks:
         print(task)
 
 
 def addTask():
     description = input("Enter a description of the task: ")
-    with open(path, 'r') as f:
-        tasks = json.load(f)
+    tasks = load_files()
     id = len(tasks) + 1
     tasks.append({"ID":id,"Description":description,"Status":"to do"})
-    with open(path, 'w') as f:
-        json.dump(tasks,f)
-
+    save_files(tasks)
 
 def updateTask():
     listTasks()
     index = int(input("Enter the ID of the task that you want to update: "))
     item = int(input("Enter (1) if you want to update the description or (2) if you want to update the status: "))
-    with open(path, 'r') as f:
-        tasks = json.load(f)
+    tasks = load_files()
     if item == 1:
         new_description = input("Eneter the new description: ")
         tasks[index-1]["Description"] = new_description
-        with open(path, 'w') as f:
-            json.dump(tasks,f)
+        save_files(tasks)
     elif item == 2:
         new_status = input("Enter the new status: ")
         tasks[index-1]["Status"] = new_status
-        with open(path, 'w') as f:
-            json.dump(tasks, f)
+        save_files(tasks)
 
 def deleteTask():
     r_id = int(input("Enter the ID of the task that you want to delete: "))
-    with open(path, 'r') as f:
-        tasks = json.load(f)
+    tasks = load_files()
     del tasks[r_id - 1]
-    with open(path, 'w') as f:
-        json.dump(tasks,f)
+    save_files(tasks)
 
 while True:
     print(menu)
@@ -76,35 +78,29 @@ while True:
     elif option == 2:
         new_status = int(input("""Enter (1) if the task is "in progress" or (2) if the task is "done": """))
         index = int(input("Enter the ID of task: "))
-        with open(path, 'r') as f:
-            tasks = json.load(f)
+        tasks = load_files()
         if new_status == 1:
             tasks[index-1]["Status"] = "in progress"
         elif new_status == 2:
             tasks[index-1]["Status"] = "done"
-        with open(path,'w') as f:
-            json.dump(tasks,f)
+        save_files(tasks)
     elif option == 3:
         listTasks()
     elif option == 4:
-        with open(path,'r') as f:
-            tasks = json.load(f)
+        tasks = load_files()
         for task in tasks:
             if task["Status"] == "done":
                 print(task)
     elif option == 5:
-        with open(path,'r') as f:
-            tasks = json.load(f)
+        tasks = load_files()
         for task in tasks:
             if task["Status"] != "done":
                 print(task)
     elif option == 6:
-        with open(path,'r') as f:
-            tasks = json.load(f)
+        tasks = load_files()
         for task in tasks:
             if task["Status"] == "in progress":
                 print(task)
     elif option == 7:
         print("Exiting program")
         break
-
